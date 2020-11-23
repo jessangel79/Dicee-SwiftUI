212e34cc743ebfbd5deea8ccdc74721cce2921e1
jessangel79 <babin.angelique@gmail.com>
2020-11-23 19:24:41 +0100

    first commit

---
+++ Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Angelique Babin on 23/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var leftDiceNumber = 1
+    @State var rightDiceNumber = 1
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("diceeLogo")
+                Spacer()
+                HStack {
+                    DiceView(number: leftDiceNumber)
+                    DiceView(number: rightDiceNumber)
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    self.leftDiceNumber = Int.random(in: 1...6)
+                    self.rightDiceNumber = Int.random(in: 1...6)
+                }) {
+                    Text("Roll")
+                        .font(.system(size: 50))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                }
+                .background(Color.red)
+            }
+            
+        }
+    }
+}
+
+struct DiceView: View {
+    let number: Int
+
+    var body: some View {
+        Image("dice\(number)")
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .padding()
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 11")
+    }
+}
